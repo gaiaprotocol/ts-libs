3ea90bd07f7a4c7be1836e09c0c7d13beca666ec
yj.gaia <gaiaprotocolnft@gmail.com>
2025-08-06 13:01:40 +0900

    chore: update code structure for improved readability and maintainability

--- packages/client-common/src/index.ts
+++ packages/client-common/src/index.ts
@@ -1,3 +1,2 @@
-export { createNameSearchResultContent } from "./components/name-search-result";
 export { createConnectButton, createRainbowKit, openWalletConnectModal, wagmiConfig } from "./components/wallet";
 
